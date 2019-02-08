aca1a3c5473e494ecdaac7ff27a7a11f5a379eac
anurajbh <anurajbh@users.noreply.github.com>
2019-02-08 12:15:43 -0500

    Formatting

--- scripts.js
+++ scripts.js
@@ -11,49 +11,53 @@ req.onload = function()
 {
 	var mUrls =[];
 	var data = JSON.parse(this.response);
-	var dataSTR = JSON.stringify(data);
 	var i = 0;
-	while(i<6)
+	while(i<4)
 	{
 		//Future work for image display
 		mUrls[i]= data.results[i].poster_path;
 		var mPoster = document.createElement("IMG");
 		mPoster.src="http://image.tmdb.org/t/p/w200"+mUrls[i];
-		mPoster.setAttribute("width", "304");
-		mPoster.setAttribute("height", "228");
+		mPoster.setAttribute("width", "100");
+		mPoster.setAttribute("height", "150");
 		mPoster.setAttribute("alt", "Movie image");
 		document.getElementById("demo").appendChild(mPoster);
-		document.getElementById("demo").innerHTML += data.results[i].title+", ";
-
+		document.getElementById("demo").innerHTML += data.results[i].title;
 		i ++;
 	}
-	document.getElementById("demo").innerHTML += data.results[i].title+".";
-	
-	/*document.getElementById("demo1").innerHTML = data.results[1].title;
-	document.getElementById("demo2").innerHTML = data.results[2].title;
-	document.getElementById("demo3").innerHTML = data.results[3].title;
-	document.getElementById("demo4").innerHTML = data.results[4].title;*/
+	/*mUrls[i]= data.results[i].poster_path;
+	mPoster.src="http://image.tmdb.org/t/p/w200"+mUrls[i];
+	mPoster.setAttribute("width", "200");
+	mPoster.setAttribute("height", "300");
+	mPoster.setAttribute("alt", "Movie image");
+	document.getElementById("demo").appendChild(mPoster);
+	document.getElementById("demo").innerHTML += data.results[i].title+".";*/
 
 }
 req.send();
 req2.onload = function()
 {
 	var data2 = JSON.parse(this.response);
-	var dataSTR2 = JSON.stringify(data2);
 	var i = 0;
 	var tUrls = [];
-	while(i<6)
+	while(i<4)
 	{
 		tUrls[i]= data2.results[i].poster_path;
 		var tPoster = document.createElement("IMG");
 		tPoster.src="http://image.tmdb.org/t/p/w200"+tUrls[i];
-		tPoster.setAttribute("width", "304");
-		tPoster.setAttribute("height", "228");
+		tPoster.setAttribute("width", "100");
+		tPoster.setAttribute("height", "150");
 		tPoster.setAttribute("alt", "TV image");
 		document.getElementById("demo2").appendChild(tPoster);
-		document.getElementById("demo2").innerHTML += data2.results[i].name+", ";
+		document.getElementById("demo2").innerHTML += data2.results[i].name;
 		i ++;
 	}
-	document.getElementById("demo2").innerHTML += data2.results[i].name+".";
+	/*tUrls[i]= data2.results[i].poster_path;
+	tPoster.src="http://image.tmdb.org/t/p/w200"+tUrls[i];
+	tPoster.setAttribute("width", "200");
+	tPoster.setAttribute("height", "300");
+	tPoster.setAttribute("alt", "Movie image");
+	document.getElementById("demo").appendChild(tPoster);
+	document.getElementById("demo2").innerHTML += data2.results[i].name;*/
 }
 req2.send();
